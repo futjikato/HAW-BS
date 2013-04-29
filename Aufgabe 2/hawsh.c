45311321db42587ae59383648c8a421cab83684e
Moritz Spindelhirn <m.spindelhirn@cashiers-check.de>
2013-04-29 11:30:08 +0200

    added some basic build in commands

--- Aufgabe 2/hawsh.c
+++ Aufgabe 2/hawsh.c
@@ -11,8 +11,20 @@ int main (void) {
 	char command[100];
 
 	while(1) {
+		// promt
 		printf("[%s] %s@%s - ", date, user, workingdir);
 		fgets(command, sizeof(command), stdin);
 
+		// remove new line
+    	command[strlen(command) - 1] = 0;
+
+		// check if command is "build-in"
+		if(strcmp(command, "quit") == 0) {
+			return 0;
+		} else if(strcmp(command, "version") == 0) {
+			printf("HAW-Shell Version 0.1 (c) TeamNahme 2013\n");
+		} else {
+			printf("TODO\n");
+		}
 	}
 }
