b02b2f361b15af641429a4e7a0bc92ae47b82a45
Vince <vince>
2013-05-13 19:17:41 +0200

    Angepasst für Windows

--- Aufgabe 2/hawsh.c
+++ Aufgabe 2/hawsh.c
@@ -1,74 +1,95 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
-#include <unistd.h>
 #include <errno.h>
+#include <direct.h>
+#include <time.h>
 
-#if _WIN32
-    #define getcwd _getcwd
-#endif // _WIN32
+#ifdef _WIN32
+#include <windows.h>
+#define USERENV "USERNAME"
+#define chdir _chdir
+#else
+#define USERENV "USER"
+#endif
 
-int main (void) {
 
-	// get current working directory
-	char* cwd = getcwd(0, 0);
+void printPrompt(char* user, char* cwd);
 
-    // get current user
-    char* user = getlogin();
 
-    // reserved for user input
-	char command[100];
+int main(int argc, char* argv[])
+{
+	char* user = getenv(USERENV);
+	char command[255];
+	char* cwd = (char*)malloc(255);
+	char* occurrence;
 
-	int running = 1;
-	while(running) {
-		// promt
-        printFormatedDate("[", "]");
-		printf(" %s@%s - ", user, cwd);
+#ifdef _WIN32
+	strcpy(cwd, argv[0]);
+	occurrence = strrchr(cwd, '\\');
+	cwd[occurrence - cwd] = 0;
+#else
+	cwd = getcwd(0, 0);
+#endif
+	
+	while (1) {
+		printPrompt(user, cwd);
 		fgets(command, sizeof(command), stdin);
-
-		// remove new line
-    	command[strlen(command) - 1] = 0;
-
-		// check if command is "build-in"
-		if(strcmp(command, "quit") == 0) {
-			running = 0;
-			puts("Bye, bye.");
-		} else if(strcmp(command, "version") == 0) {
+		command[strlen(command) - 1] = 0;
+		if (strcmp(command, "quit") == 0) {
+			printf("Bye, bye.\n");
+			return 0;
+		} else if (strcmp(command, "version") == 0) {
 			printf("HAW-Shell Version 0.1 (c) TeamNahme 2013\n");
-		} else if(command[0] == '/') {
-			if(chdir(command) != 0) {
+		} else if (command[0] == '/') {
+#ifdef _WIN32
+			char tmp[255];
+			strcpy(tmp, command + 1);
+			if (chdir(tmp) != 0) {
+				printf("Unable to change dir : %s\n", strerror(errno));
+			} else {
+				strcpy(cwd, tmp);
+			}
+#else
+			if (chdir(command) != 0) {
 				printf("Unable to change dir : %s\n", strerror(errno));
 			} else {
 				cwd = command;
 			}
-		} else if(command[strlen(command) - 1] == '&') {
+#endif
+		} else if (command[strlen(command) - 1] == '&') {
 			// create new thread for command
 			puts("Start new thread for given command");
 			command[strlen(command) - 1] = 0;
+		} else if (strlen(command) == 0) {
+			continue;
 		} else {
+#ifdef _WIN32
+			wchar_t wtext[255];
+			STARTUPINFO info;
+			PROCESS_INFORMATION processInfo;
+			mbstowcs(wtext, command, strlen(command));
+			if (CreateProcess(wtext, L"", NULL, NULL, TRUE, 0, NULL, NULL, &info, &processInfo)) {
+				WaitForSingleObject(processInfo.hProcess, INFINITE);
+				CloseHandle(processInfo.hProcess);
+				CloseHandle(processInfo.hThread);
+			}
+#else
 			int pid = fork();
 			if(pid == 0) {
 				execve(command, 0, 0);
 			} else {
 				waitpid(pid, 0, 0);
-			} 
+			}
+#endif
 		}
 	}
 
 	return 0;
 }
-
-/**
- * Print a formated date string
- */
-int printFormatedDate (char* prefix, char* suffix) {
-    time_t ct;
-    struct tm *ts;
-    
-    ct = time(NULL);
-    ts = localtime(&ct);
-    
-    printf("%s%d.%d.%d %d:%d%s", prefix, ts->tm_mday, ts->tm_mon, ts->tm_year + 1900, ts->tm_hour, ts->tm_min, suffix);
-    return 0;
+void printPrompt(char* user, char* cwd)
+{
+	time_t rawtime = time(NULL);
+	struct tm* now = localtime(&rawtime);
+	printf("[%02d:%02d][%s:%s]# ", now->tm_hour, now->tm_min, user, cwd);
 }
